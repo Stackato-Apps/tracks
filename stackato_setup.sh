10bda35d0443bf4dfe934ae9738e485860a206de
Billy Tat <billy.tat@hpe.com>
2015-12-04 10:45:46 -0800

    Fix sed command

--- stackato_setup.sh
+++ stackato_setup.sh
@@ -6,11 +6,11 @@ if [ $STACKATO_HOOK_ACTION == 'create' ] && [ ! -f config/site.yml ]; then
 
   echo "Generating salt..."
   salt=$(ruby -e "require 'bcrypt'; puts BCrypt::Engine.generate_salt")
-  sed -n "s|salt: \"change-me\"|salt: \"$salt\"|" config/site.yml
+  sed -i "s|salt: \"change-me\"|salt: \"$salt\"|" config/site.yml
 
   echo "Generating secret token..."
   secret=$(rake secret)
-  sed -n "s|secret_token: \"change-me\"|secret_token:\"$secret\"|" config/site.yml
+  sed -i "s|secret_token: \"change-me\"|secret_token: \"$secret\"|" config/site.yml
 else
   echo "config/site.yml exists"
 fi
